e617265ac8fce9c483d97fc89082a151b2d4a51b
PALLA VAISHNAVI <46565534+vaishnavipalla@users.noreply.github.com>
2019-04-22 21:51:09 +0530

    Add files via upload

---
+++ cheby2.py
@@ -0,0 +1,15 @@
+import numpy as np
+from scipy import signal
+import matplotlib.pyplot as plt
+b,a=signal.cheby2(4,40,100,'low',analog=True)
+w,h=signal.freqs(b,a)
+plt.semilogx(w,20*np.log10(abs(h)))
+plt.title('chebyshev Type II frequency response(rs=40)')
+plt.xlabel('Frequency [radians/second]')
+plt.ylabel('Amplitude[dB]')
+plt.margins(0,0.1)
+plt.grid(which='both',axis='both')
+plt.axvline(100,color='green')#cutoff frequency
+plt.axhline(-40,color='green')#rs
+plt.show()
+
